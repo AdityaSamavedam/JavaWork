7e7fae9251a45684ac26978d1a85811aab47cbe8
Sheetal <sheetal.srikumar@gmail.com>
2024-09-25 21:26:36 -0700

    saving changes

---
+++ ArrayList/InsertionSort/Solved/InsertionSort.java
@@ -0,0 +1,40 @@
+package ArrayList.InsertionSort.Solved;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 9, 1, 5, 6};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    // Method to perform insertion sort
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i]; // Current element to be inserted
+            int j = i - 1;
+
+            // Move elements of array[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            // Insert the key at the correct position
+            array[j + 1] = key;
+        }
+    }
+
+    // Utility method to print the array
+    public static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
