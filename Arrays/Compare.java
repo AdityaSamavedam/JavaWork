4f04a304c110fcceaba3721b290c4fe808533546
Sheetal <sheetal.srikumar@gmail.com>
2024-09-25 20:55:37 -0700

    saving compare

--- Arrays/Compare.java
+++ Arrays/Compare.java
@@ -1,9 +1,9 @@
-package Compare.Unsolved;
-
 public class Compare {
     public static void main(String[] args) {
-        // Returns 0 if the arrays are equal.
-        // Returns a positive integer if array2 is greater than array3
-         // Returns a negative integer if array2 is lesser than array 3 than array3
+        
+                // Returns 0 if the arrays are equal.
+                // Returns a positive integer if array2 is greater than array3
+                 // Returns a negative integer if array2 is lesser than array 3 than array3
+       
     }
 }
