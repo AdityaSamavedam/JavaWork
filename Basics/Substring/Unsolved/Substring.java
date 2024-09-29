d5139ca0c5d7a1e57b0fae90590106e1a643749b
Sheetal <sheetal.srikumar@gmail.com>
2024-09-29 09:23:57 -0700

    saving changes

---
+++ Basics/Substring/Unsolved/Substring.java
@@ -0,0 +1,9 @@
+package Basics.Substring.Unsolved;
+
+public class Substring {
+    public static void main(String[] args) {
+        //Substring from a starting index to the end:
+
+        //Substring from a starting index to an ending index:
+    }
+}
