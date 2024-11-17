d1c8a2d0f5ef7ba275baf835d36b80273c0f0895
Aditya Samavedam <adityasam1357@gmail.com>
2024-11-17 11:15:04 -0800

    employee test error

--- Basics/Polymorphism/Main.java
+++ Basics/Polymorphism/Main.java
@@ -1,31 +1,27 @@
 package Basics.Polymorphism;
 
-import java.util.ArrayList;
-
 class Main {
     public static void main(String[] args) {
-        Animal myAnimal = new Animal(); // Create a Animal object
-        Animal myPig = new Pig(); // Create a Pig object
-        Animal myDog = new Dog(); // Create a Dog object
-        myAnimal.animalSound();
-        myPig.animalSound();
-        myDog.animalSound();
-        // Polymorphic arguments and return types
-        Dog d = new Dog();
-        Pig p = new Pig();
-        myAnimal.giveshot(d);// dog's makenoise runs
-        myAnimal.giveshot(p);// pigs makenoise runs
-        
-        // The giveShot() method can take any
-        // Animal you give it. As long as the object
-        // you pass in as the argument is a subclass of
-        // Animal, it will work
-        ArrayList<Shapes> myShapes = new ArrayList<>();
-        myShapes.add(new Circle());
-        myShapes.add(new Rectangle());
-        myShapes.add(new Triangle());
-        for (Shapes eachShape : myShapes) {
-            eachShape.draw();
-        }
+        // Animal myAnimal = new Animal(); // Create a Animal object
+        // Animal myPig = new Pig(); // Create a Pig object
+        // Animal myDog = new Dog(); // Create a Dog object
+        // myAnimal.animalSound();
+        // myPig.animalSound();
+        // myDog.animalSound();
+        // Dog d = new Dog();
+        // Pig p = new Pig();
+        // myAnimal.giveshot(d);
+        // myAnimal.giveshot(p);
+        // ArrayList<Shapes> myShapes = new ArrayList<>();
+        // myShapes.add(new Circle());
+        // myShapes.add(new Rectangle());
+        // myShapes.add(new Triangle());
+        // for (Shapes eachShape : myShapes) {
+        //     eachShape.draw();
+        // }
+        Employee manager = new Manager("Jason", 10000, 5);
+        Employee developer = new Developer("Jack", 13000, "Java");
+        manager.displayDetails();
+        developer.displayDetails();
     }
 }
